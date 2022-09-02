d20f38837f54de00bcf16e8ec3b6dc42b359006a
Alexander Rubinstein <zeevwolf@live.com>
2022-09-03 00:10:18 +0300

    added gatsby-source-filesytem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,18 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: `${__dirname}/src/pages`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images`
+      }
+    },
+  ],
 }
